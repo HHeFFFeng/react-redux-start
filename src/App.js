d4be69055f58f2510d200ae88f90495c3a0ad426
HHeFFFeng <hefeng_2010@163.com>
2022-03-09 16:03:22 +0800

    react
    通过参数形式传递

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        redux
+        当前值: {this.props.value}
+        <p>
+          <button onClick={ this.props.onIncrement }>加+</button>
+          <button onClick={ this.props.onDecrement }>减-</button>
+        </p>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import reducer from "./reducers/counter"
+import { createStore } from 'redux';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 创建仓库
+const store = createStore(reducer)
+
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App 
+        value = {store.getState()}
+        onIncrement={ () => store.dispatch({ type: "INCREMENT" }) }
+        onDecrement={ () => store.dispatch({ type: "DECREMENT" }) }
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
+
+render();
+
+// 监听到数据的变化
+store.subscribe(render)
